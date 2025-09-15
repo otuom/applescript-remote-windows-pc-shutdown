dddf13a9449a0db88ced97c54c184358d543343f
otuom <oscar.tuominen@riksteatern.se>
2025-09-15 12:43:17 +0200

    Update setup-mac.sh

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,1 +1,5 @@
+# Install Homebrew macOS Package Manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Samba suite
 brew install samba
